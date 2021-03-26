7a3f499d48841f9afd48f375f58563ea11da45c0
Pawlus207 <79367739+Pawlus207@users.noreply.github.com>
2021-03-26 14:42:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-const {Collection, Client, Discord} = require('discord.js')
-const fs = require('fs')
-const client = new Client({
-    disableEveryone: true
+const discord = require('discord.js');
+
+const bot = new discord.Client();
+
+bot.on("ready", () => {
+    console.log("The bot is ready and online!")
 })
-const config = require('./config.json')
-const prefix = config.prefix
-const token = config.token
-client.commands = new Collection();
-client.aliases = new Collection();
-client.categories = fs.readdirSync("./commands/");
-["command"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-}); 
-client.on('ready', () => {
-    client.user.setActivity(`${prefix}help`)
-    console.log(`${client.user.username} ✅`)
-})
-client.on('message', async message =>{
-    if(message.author.bot) return;
-    if(!message.content.startsWith(prefix)) return;
-    if(!message.guild) return;
-    if(!message.member) message.member = await message.guild.fetchMember(message);
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if(cmd.length == 0 ) return;
-    let command = client.commands.get(cmd)
-    if(!command) command = client.commands.get(client.aliases.get(cmd));
-    if(command) command.run(client, message, args) 
-})
-client.login(token)
+
+bot.login('ODI0OTkyOTUwNzYwMzc0MzMz.YF3cjw.qO2qUkLddsYyVovU6-InBw5PVwc')
